08d777c2fb09759d1ce1c1896d3c24087a5736f4
Yalcin <bilgekagan902@gmail.com>
2024-03-01 08:33:56 +0300

    array methods

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace arrayMethod{
+      class Program{
+            public static void Main(string[] args){
+                  int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+                  Console.WriteLine("*** sırasız dizi");
+                  foreach(var sayi in sayiDizisi){
+                        Console.WriteLine(sayi);
+                  }
+                  Console.WriteLine("sirali dizi");
+                  Array.Sort(sayiDizisi);
+
+                  foreach(var sayi in sayiDizisi){
+                        Console.WriteLine(sayi);
+                  }
+                  Array.Clear(sayiDizisi,2,2);
+                  foreach(var sayi in sayiDizisi){
+                        Console.WriteLine(sayi);
+                  }
+                  Array.Reverse(sayiDizisi);
+                  foreach(var sayi in sayiDizisi){
+                        Console.WriteLine(sayi);
+                  }
+                  Array.IndexOf(sayiDizisi,23);
+                  Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+                  Array.Resize<int>(ref sayiDizisi,9);
+                  sayiDizisi[8] = 99;
+                  foreach(var sayi in sayiDizisi){
+                        Console.WriteLine(sayi);
+                  }
+            }
+      }
+}
